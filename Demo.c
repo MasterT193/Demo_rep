30a4fe1f222bf3ea496b9924e591fdfc123c7a39
MasterT193 <mastertanishq193@gmail.com>
2025-01-28 12:48:54 +0530

    Create Demo.c

---
+++ Demo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  int a, b, c;
+  printf("Enter the FIRST number:");
+  scanf("%d", &a);
+  printf("Enter the SECOND number:");
+  scanf("%d", &b);
+  c = a + b;
+  printf("Sumof two numbers is %d.", c);
+  return 0;
+}
